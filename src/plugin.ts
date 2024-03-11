1ede8ad415cb0c2bd6325ce3cf236284e1d64f90
Teddi <teddi.orange@gmail.com>
2024-03-11 21:26:39 +0000

    Remove some commentary / debug + add dependabot

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,13 +1,6 @@
 import streamDeck, { LogLevel } from "@elgato/streamdeck";
-
 import { PowerMonitor } from "./actions/power-monitor";
 
-// We can enable "trace" logging so that all messages between the Stream Deck, and the plugin are recorded. When storing sensitive information
 streamDeck.logger.setLevel(LogLevel.ERROR);
-console.log("Starting plugin");
-
-// Register the increment action.
 streamDeck.actions.registerAction(new PowerMonitor());
-
-// Finally, connect to the Stream Deck.
 streamDeck.connect();
