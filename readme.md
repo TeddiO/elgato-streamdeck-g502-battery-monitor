f41a6c75fe39bdb21a60914fee54ca2f5617d6df
Teddi <teddi.orange@gmail.com>
2024-03-11 21:19:43 +0000

    Attempt to re-query if we dont initialize correctly, add default images

--- readme.md
+++ readme.md
@@ -2,8 +2,9 @@
 
 A plugin for the Stream Deck that allows for monitoring the battery level of a Logitech G502 Lightspeed mouse (and variants).
 
-This project is currently a work in progress and needs some tidying, but does otherwise work.
+Upon boot or back to the page with the indicator on, it'll query the battery level and update the charge accoringly. It'll also give you an indicator for when it's charging and / or fully charged, as well as the warning icon for when the battery level drops below 20%.
 
+- WIP, needs some custom iconography / backrounds.
 - Requires the Logitech G-Hub to be installed.
 - Based on the beta NodeJS SDK for the Stream Deck (https://github.com/elgatosf/streamdeck)
 - Requires Stream Deck 6.4 or newer.
--- src/actions/power-monitor.ts
+++ src/actions/power-monitor.ts
@@ -1,15 +1,24 @@
-import { action, SingletonAction, WillAppearEvent } from "@elgato/streamdeck";
+import { action, SingletonAction, WillAppearEvent, WillDisappearEvent } from "@elgato/streamdeck";
 import streamDeck, { LogLevel } from "@elgato/streamdeck";
 import WebSocket from 'ws';
 
+const logLevels: { [key: string]: LogLevel } = {
+	'debug': LogLevel.DEBUG,
+	'info': LogLevel.INFO,
+	'warn': LogLevel.WARN,
+	'error': LogLevel.ERROR,
+	'trace': LogLevel.TRACE
+};
+
+const logLevel = process.env.BATTERY_MONITOR_LOGLEVEL ? logLevels[process.env.BATTERY_MONITOR_LOGLEVEL.toLowerCase()] : LogLevel.ERROR;
 const logger = streamDeck.logger.createScope("Websocket Log");
-logger.setLevel(LogLevel.TRACE);
+logger.setLevel(logLevel);
 
 let regex = /g502/g;
 let deviceID: string = "";
 let ws: WebSocket;
 let curBattery: number = 0;
-
+let initialized: boolean = false;
 
 function GetBatteryLevel(deviceID: string) {
 	ws.send(JSON.stringify(
@@ -31,11 +40,34 @@ function SubscribeToBatteryLevel() {
 	);
 }
 
+const states: { [key: string]: string } = { "charging": "⚡", "fullyCharged": "🔋" };
+function HandleChargeString(dataMap: any) {
+	
+	if (dataMap.payload.fullyCharged) {
+		return "🔋100%";
+	}
+
+	if (dataMap.payload.charging) {
+		return `⚡${curBattery}%`;
+	}
+
+	return `${curBattery}%`;
+}
+
 @action({ UUID: "com.teddi.g502-battery-monitor.increment" })
 export class PowerMonitor extends SingletonAction {
 	
 	onWillAppear(ev: WillAppearEvent<any>): void | Promise<void> {
 		ws = new WebSocket('ws://localhost:9010', "json");
+
+		setTimeout(() => {
+			if (!initialized) {
+				ws.close();
+				logger.debug("Attempting to close WebSocket connection and re-init...");
+				this.onWillAppear(ev);
+				return;
+			}
+		}, 5000);
 
 		ws.on('open', function open() {
 			logger.debug("Connected to G-HUB WebSocket");
@@ -48,44 +80,62 @@ export class PowerMonitor extends SingletonAction {
 					"path": "/devices/list" 
 				})
 			);
+			logger.debug("Sent request for /devices/list");
 		});
 
 		ws.on('message', function message(data) {
+			logger.debug(`Received message from G-HUB`);
 			let dataMap = JSON.parse(data.toString());
-
-			// TODO: Iterate through the device list and find a G502.
-			if (dataMap.path === "/devices/list" && (regex.test(dataMap.payload.deviceInfos[0].deviceModel))) {
+	
+			if (dataMap.path === "/devices/list") {
 				logger.debug("Received /devices/list");
-				deviceID = dataMap.payload.deviceInfos[0].id;
-				logger.debug(`Device ID: ${deviceID}`);
-				GetBatteryLevel(deviceID)
+				for (let device of dataMap.payload.deviceInfos) {
+					if (regex.test(device.deviceModel)) {
+						deviceID = device.id;
+						logger.debug(`Device ID: ${deviceID}`);
+						GetBatteryLevel(deviceID)
+						initialized = true;
+						return;
+					}
+				}
 			}
 
 			if (dataMap.path === `/battery/${deviceID}/state`) {
 				logger.debug(`Received /battery/${deviceID}/state`);
 				curBattery = dataMap.payload.percentage;
 				SubscribeToBatteryLevel();
-				ev.action.setTitle(`${curBattery}%`);
+				ev.action.setTitle(HandleChargeString(dataMap));
+				return;
 			}
 
 			if (dataMap.path === "/battery/state/changed") {
-				// Need to wait for the event...
 				logger.debug(`Received /battery/state/changed`);
 				if (dataMap.payload.deviceId === deviceID) {
 					curBattery = dataMap.payload.percentage;
-					ev.action.setTitle(`${curBattery}%`);
+					ev.action.setTitle(HandleChargeString(dataMap));
+
+					if (curBattery <= 20) {
+						ev.action.showAlert()
+					}
 				}
 			}
+			return;
+		});
+		
 
-	
+		ws.on('error', function error(err) {
+			logger.error(`WebSocket error: ${err}`);
 		});
 
-		return ev.action.setTitle("Pending information...");
+		
+		return ev.action.setTitle("...%");
 	}
 
-	// TODO: Deal with the fact where if we change screen, we'll want to kill the websocket connection.
-	// onWillDisappear(ev: WillAppearEvent<any>): void | Promise<void> {
-
-	// }
+	onWillDisappear(ev: WillDisappearEvent<any>): void | Promise<void> {
+		logger.debug("Closing WebSocket connection");
+		ws.close();
+		initialized = false;
+		logger.debug("WebSocket connection closed");
+	}
 }
 
